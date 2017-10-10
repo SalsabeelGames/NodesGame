7cf1d43f79e3e0d650c678bc5357d796b668e52f
mhnd-abu-safia <mhnd.abu.safia@hotmail.com>
2017-10-10 23:31:03 +0300

    enhancement on result dialog

--- Assets/Assets/Scripts/ResultDialog.cs
+++ Assets/Assets/Scripts/ResultDialog.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
-public class ResultDialog : MonoBehaviour {
+public class ResultDialog : MonoBehaviour
+{
     public float imageXScale;
     public float imageYScale;
 
@@ -28,5 +31,20 @@ public class ResultDialog : MonoBehaviour {
             }
             this.gameObject.transform.Find("front_image").gameObject.transform.localScale = new Vector3(imageXScale, imageYScale, 0);
         }
+    }
+
+    void setStar(int starNumber, bool isOn)
+    {
+        //name of star images should start from number 1.
+        if(starNumber > 0)
+        {
+            GameObject star = this.gameObject.transform.Find("front_image").gameObject.transform.Find("star_" + starNumber).gameObject;
+            star.SetActive(isOn);
+        }
+    }
+
+    public void onRestartClick()
+    {
+        SceneManager.LoadScene("GameScene");
     }
 }
