ebe7f43de3add6aa1fb0a696d99fed99297bba37
abdRumman <32413587+abdRumman@users.noreply.github.com>
2017-10-10 23:52:36 +0300

    Go To Next Level

--- Assets/Assets/Scripts/GameCamera.cs
+++ Assets/Assets/Scripts/GameCamera.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameCamera : MonoBehaviour {
 	public GameObject cv;
-	// Use this for initialization
-	void Start () {
+	public Text textTime;
+	public string numberLevel;
+	float timeLeft = 30.0f;
 
+	void Start () {
+		
 		string LevelToLoadNumber = PlayerPrefs.GetString ("LevelToLoad");
 		cv.gameObject.transform.Find ("level_" + LevelToLoadNumber).gameObject.SetActive (true);
+
+
+		if(LevelToLoadNumber == "1"){
+			timeLeft = 30.0f;
+		}else if(LevelToLoadNumber == "2"){
+			timeLeft = 40.0f;
+		}
 
         if (this.gameObject.GetComponent<AudioSource>() != null)
         {
@@ -23,15 +36,30 @@ public class GameCamera : MonoBehaviour {
         }
     }
 	
-	float timeLeft = 30.0f;
+
 	//Text outTime = ""
 	void Update()
 	{
 		timeLeft -= Time.deltaTime;
-		Debug.Log (timeLeft.ToString("0.00").Replace(".",":"));
+		textTime.text = timeLeft.ToString ("0.00").Replace (".", ":");
 		if(timeLeft < 0)
 		{
 			
+		}
+	}
+
+	public void GoToNextLevel(){
+		string LevelToLoadNumber = PlayerPrefs.GetString ("LevelToLoad");
+		if (LevelToLoadNumber == numberLevel) {
+
+		}else{
+			int nextLevelNumber = (Int32.Parse (LevelToLoadNumber) + 1);
+			PlayerPrefs.SetString ("LevelToLoad",""+nextLevelNumber);
+			int LastOpenLevel = Int32.Parse (PlayerPrefs.GetString ("LastOpenLevel"));
+			if(LastOpenLevel < nextLevelNumber){
+				PlayerPrefs.SetString ("LastOpenLevel","" +nextLevelNumber );
+			}
+			SceneManager.LoadScene("GameScene");
 		}
 	}
 }
